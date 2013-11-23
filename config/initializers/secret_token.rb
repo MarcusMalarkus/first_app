765d513049f523e6921ea2b6fb03a5918cf8c80b
Marcus <apprentice@Marcus.local>
2013-11-22 18:13:19 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hartl::Application.config.secret_token = '89b0b35f4d58bcff8a35aa1ad59938141e6286f3684e64184a936c8a5a292ae209b6ace0f1a796724b7e2ec0c9633001191056e5752de7d88485635a7e1e6c5e'
